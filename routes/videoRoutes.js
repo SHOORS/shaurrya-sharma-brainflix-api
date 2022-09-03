04cc39f0031effa991db4ed73a9605adf2a87587
Shaurrya <shaurrya@hotmail.com>
2022-09-03 16:18:36 -0400

    added env, routes, and static images

---
+++ routes/videoRoutes.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const cors = require("cors");
+var uniqid = require("uniqid");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+const router = express.Router();
+app.use(express.json());
+app.use("/images", express.static("./public/images"));
+
+const readVideo = (fileName) => {
+    const fileContent = JSON.parse(fs.readFileSync(`./data/${fileName}.json`));
+    return fileContent;
+  };
+  
+
+router.get("/", function (req, res) {
+    const videos = readVideo("videos");
+    res.json(videos);
+});
+
+router.get("/:id", (req, res) => {
+    const videos = readVideo("videos");
+    const video = videos.find((vid) => vid.id === req.params.id);
+    res.send(video);
+});
+
+router.post("/upload", function (req, res) {
+    const videos = readVideo("videos");
+    const newVideo = {
+      title: req.body.title,
+      channel: "Shoors",
+      image: `https://source.unsplash.com/random/300x200?sig=${Math.random()}`,
+      description: req.body.description,
+      views: "7654357",
+      likes: "9001",
+      duration: "4:19",
+      video: "https://project-2-api.herokuapp.com/stream",
+      timestamp: Date.now(),
+      comments: [
+        {
+          name: "Shaurrya Sharma",
+          comment:
+            "It’s like a weight has been lifted off of my shoulders. Thank you so much for sharing this video. I will be sending this to all of my friends and family ASAP.",
+          likes: 0,
+          timestamp: Date.now(),
+        },
+        {
+          name: "SHOORS",
+          comment:
+            "Let’s collaborate on a video for making more videos! I’ll have my associates contact yours.",
+          likes: 0,
+          timestamp: Date.now(),
+        },
+      ],
+      id: uniqid(),
+    };
+    videos.push(newVideo);
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
+    res.json(videos);
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,72 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require("fs");
+
 const cors = require("cors");
-var uniqid = require('uniqid'); 
-var bodyParser = require('body-parser')
+
+var bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static("./public/images"));
+const videoRoutes = require("./routes/videoRoutes");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
 
-app.listen(8080, () => {
-    console.log('Server Started on http://localhost:8080');
-    console.log('Press CTRL + C to stop server');
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const readVideo = (fileName) => {
-    const fileContent = JSON.parse(fs.readFileSync(`./data/${fileName}.json`));
-    return fileContent;
-}
-
-app.get("/videos", function(req, res) {
-    const videos = readVideo("videos");    
-    res.json(videos);
-})
-
-app.get('/videos/:id', (req, res) => {
-    const videos = readVideo("videos");
-    const video = videos.find(vid => vid.id === req.params.id);
-    res.send(video);
-});
-
-app.post("/upload", function(req, res) {
-    const videos = readVideo("videos");
-    const newVideo = {
-        "title": req.body.title,
-        "channel": "Shoors",
-        "image": "https://i.imgur.com/l2Xfgpl.jpg",
-        "description": req.body.description,
-        "views": "80085",
-        "likes": "over 9000",
-        "duration": "4:20",
-        "video": "https://project-2-api.herokuapp.com/stream",
-        "timestamp": Date.now(),
-        "comments": [
-            {
-            "name": "Micheal Rideout",
-            "comment": "Walnut donuts!",
-            "likes": 0,
-            "timestamp": Date.now()
-            },
-            {
-            "name": "Dr Wong",
-            "comment": "Wassup!",
-            "likes": 0,
-            "timestamp": Date.now()
-            },
-            {
-            "name": "Cat",
-            "comment": "Cat",
-            "likes": 0,
-            "timestamp": Date.now()
-            }
-        ],
-        "id": uniqid()
-    }     
-    videos.push(newVideo);
-    fs.writeFileSync("./data/videos.json", JSON.stringify(videos));    
-    res.json(videos);
-}) 
+app.use("/videos", videoRoutes)
 
 
